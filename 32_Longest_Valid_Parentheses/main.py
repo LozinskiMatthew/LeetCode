c53a22f3adb4f08a65314c0af40e3743e0be0241
Matthew Lozinski <lozinskimatthew0@gmail.com>
2024-09-07 09:36:55 +0200

    Couldnt resolve the issue in 32 hard leetcode challenge max_len always equals to 0

--- 32_Longest_Valid_Parentheses/main.py
+++ 32_Longest_Valid_Parentheses/main.py
@@ -1,20 +1,31 @@
 class Solution:
-    # ))) (
+    def edit_length(self, max_len, curr_len):
+        if max_len < curr_len:
+            max_len = curr_len
+            curr_len = 0
+        return max_len, curr_len
+
     def longestValidParentheses(self, s: str) -> int:
         max_len = 0
         curr_len = 0
         stack = []
-        n = False
         for char in s:
             if char == ")":
-                if len(stack) != 0:
-                    top_element = stack.pop()
+                if len(stack) == 0:
+                    max_len, curr_len = self.edit_length(max_len, curr_len)
+                    curr_len = 0
                 else:
-
-                elif:
-
+                    curr_len += 1
+                    stack = []
+            else:
+                if len(stack) == 1:
+                    max_len, curr_len = self.edit_length(max_len, curr_len)
+                    stack.append("(")
+                    curr_len = 0
                 else:
-                n = False
+                    stack.append("(")
+        return max_len * 2
 
-        else:
-            stack.append(char)
+if __name__ == "__main__":
+    solve = Solution()
+    print(solve.longestValidParentheses("(()"))  # 2
