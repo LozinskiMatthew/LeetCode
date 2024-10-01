fd460fc17fc67a09d8edb4b3cbdf5ac3b07716c8
Matthew Lozinski <lozinskimatthew0@gmail.com>
2024-10-01 23:37:15 +0200

    Daily challenge

---
+++ 1497_Array_Pairs/main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        remainder_count = {}
+        for num in arr:
+            remainder = ((num % k) + k) % k
+            remainder_count[remainder] = remainder_count.get(remainder, 0) + 1
+
+        for rem, count in remainder_count.items():
+            if rem == 0:
+                if count % 2 != 0:
+                    return False
+            else:
+                complement = k - rem
+                if complement not in remainder_count or remainder_count[complement] != count:
+                    return False
+
+        return True
